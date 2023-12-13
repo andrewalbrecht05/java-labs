559caa0a330a5641d2664eebf316e697a663427d
andrewalbrecht05 <andrijalbrext@gmail.com>
2023-12-13 14:42:16 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,53 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
- * Клас Main відповідає за взаємодію з користувачем.
+ * Клас, який демонструє обробку введених користувачем чи згенерованих випадковим чином чисел та їхнє виведення в зворотньому порядку.
  */
 public class Main {
 
+    /**
+     * Точка входу в програму.
+     *
+     * @param args аргументи командного рядка (не використовуються в даному випадку)
+     */
     public static void main(String[] args) {
-        Storage storage = new Storage();
+        // Запитати в користувача число
         Scanner scanner = new Scanner(System.in);
-        // Додати список дозволених речей
-        List<String> allowedItems = Arrays.asList("сумка", "коробка", "куртка", "ложка");
-        System.out.println("Якщо бажаєте зупинити программу, введіть -1");
-        while (true) {
-            // Запросити у користувача, що він хоче зробити
-            System.out.println("Що ви хочете зробити?\n1. Записати річ\n2. Переглянути вміст комірки");
-            int choice = scanner.nextInt();
-            if (choice == -1) {
-                break;
+        System.out.println("Введіть число: ");
+        int count = scanner.nextInt();
+
+        System.out.println("Чи бажаєте ви зчитувати числа (0 - ні, 1 - так)");
+        int choice = scanner.nextInt();
+
+        // Створити список чисел
+        ArrayList<Integer> numbers = new ArrayList<>();
+        if (choice == 0) {
+            for (int i = 0; i < count; i++) {
+                int x = (int) (Math.random() * 100);
+                numbers.add(x);
             }
-
-            if (choice == 1) {
-                System.out.println("Введіть номер рядка комірки: ");
-                int row = scanner.nextInt();
-                System.out.println("Введіть номер стовпця комірки: ");
-                int column = scanner.nextInt();
-                // Запросити у користувача дані про речі, які він хоче зберегти
-                System.out.println("Введіть назву речі: ");
-                String itemName = scanner.next();
-
-                // Перевірити, чи дозволена річ
-                if (!allowedItems.contains(itemName)) {
-                    System.out.println("Ця річ не дозволена");
-                    continue;
-                }
-
-                // Зберегти річ у комірку
-                try {
-                    storage.store(row, column, itemName);
-                    System.out.println("Предмет успішно збережений");
-                } catch (CellDoesntExistsException | CellOccupiedException e) {
-                    System.out.println("Помилка зберігання комірки!");
-                }
-            } else if (choice == 2) {
-                System.out.println("Введіть номер рядка комірки: ");
-                int row = scanner.nextInt();
-                System.out.println("Введіть номер стовпця комірки: ");
-                int column = scanner.nextInt();
-                // Отримати вміст комірки
-                try {
-                    Object content = storage.getCell(row, column).getContent();
-                    if (content == null) {
-                        System.out.println("Комірка порожня");
-                    } else {
-                        System.out.println("Вміст комірки: " + content);
-                    }
-                } catch (CellDoesntExistsException e) {
-                    System.out.println("Комірка не існує");
-                }
-            } else {
-                System.out.println("Неправильний вибір");
+        } else {
+            for (int i = 0; i < count; i++) {
+                System.out.println("Введіть число " + (i + 1) + ": ");
+                int x = scanner.nextInt();
+                numbers.add(x);
             }
+        }
+
+        // Вивести початковий масив
+        System.out.println("Початковий масив: ");
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i) + " ");
+        }
+        System.out.println();
+
+        // Відобразити числа в зворотньому порядку
+        Collections.reverse(numbers);
+        System.out.println("Масив у зворотньому порядку: ");
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i) + " ");
         }
     }
 }
